50e29ea3da234f7164b387210b47752cf9f20fde
Edward <ewalbach@gmail.com>
2020-04-15 20:45:33 +0300

    init

---
+++ Person.cpp
@@ -0,0 +1,24 @@
+
+#include "Person.h"
+
+using namespace std;
+
+Person::Person(string first,string last,
+	int arbitrary) : firstname(first),lastname(last),
+	arbitrarynumber(arbitrary)
+{
+}
+
+
+
+string Person::GetName() const
+{
+	return firstname + " " + lastname;
+}
+
+
+void Person::AddResource()
+{
+	pResource.reset();
+	pResource=std::make_shared<Resource>("Resource for " + GetName());
+}
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "Person.h"
+#include <string>
+using std::string;
+
+int Dummy(Person p) { return p.GetNumber(); }
+
+int main()
+{
+    {
+        Person Kate("Kate", "Gregory", 345);
+        Kate.AddResource();
+        string s1 = Kate.GetResourceName();
+        Kate.AddResource();
+        Person Kate2 = Kate;
+        Kate = Kate2;
+        int j = Dummy(Kate2);
+    }
+    return 0;
+}
